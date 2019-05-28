bddff2636019d0a55dab1aceccbdba1cbb547fe7
Kaleb Burris <kaleb.r.burris@gmail.com>
2019-05-28 00:03:19 -0800

    baseWhatever
    Yeah, I know the code is inconsistent. Mild fixes, and added baseWhatever, which goes to base 36. Have fun.

--- src/With Prints (Slow)/baseWhateverPrints.java
+++ src/With Prints (Slow)/baseWhateverPrints.java
@@ -1,11 +1,19 @@
 import java.util.*;
 import java.math.*;
 
-public class baseSixteenPrints {
+public class baseWhateverPrints {
 	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Base: ");
+		int base = input.nextInt();
+		int check = 10;
+		if(base > 10){
+			check = base;
+		}
+		System.out.println();
 		int result = 0; // The number of persistence
-		BigInteger nums = new BigInteger("1" , 16); // Essentially, a REALLY big integer
-		BigInteger multi = new BigInteger("0", 16); // Math stuffs for multiplying the digits
+		BigInteger nums = new BigInteger("1" , base); // Essentially, a REALLY big integer
+		BigInteger multi = new BigInteger("0", base); // Math stuffs for multiplying the digits
 		String stringTotal = ""; // Used to store a converted nums value
 		int MAXSTEPS = 0; // The current maximum persistence
 		String MAXSTEPS_NUMBER = "0"; // The lowest number for the highest reached persistence
@@ -13,41 +21,41 @@ public class baseSixteenPrints {
 		int currLeng = 1;
 		int temp = 2;
 		here: while(true){ // The "here:" is for later, establishing a functional loop
-			multi = new BigInteger("f", 16); // Setting to a value that works
-			stringTotal = nums.toString(16); // Converting
+			multi = new BigInteger("10", base); // Setting to a value that works
+			stringTotal = nums.toString(base); // Converting
 			result = 0;
 			temp = 2;
 			for (int t = 0; t < stringTotal.length(); t++) {
 				if (temp > Character.getNumericValue(stringTotal.charAt(t))){
-					nums = nums.add(new BigInteger(thing.giveMe(stringTotal.length() - t - 1), 16));
+					nums = nums.add(new BigInteger(thing.giveMe(stringTotal.length() - t - 1), base));
 					continue here;
 				}
 				temp = Character.getNumericValue(stringTotal.charAt(t));
 				switch(stringTotal.charAt(t)){ // Skips if there's a 1 or 0
 					case '1': // It's the same number without the digit, which we've already checked
 					case '0': // Death
-						nums = nums.add(new BigInteger(thing.giveMe(stringTotal.length() - t - 1), 16));
+						nums = nums.add(new BigInteger(thing.giveMe(stringTotal.length() - t - 1), base));
 						continue here;
 				}
 			}
 			System.out.println(stringTotal);
-			while(multi.compareTo(new BigInteger("e", 16)) == 1) { // "multi.compareTo(" returns 1 if the number being used to compare is larger
-				multi = new BigInteger("1", 16);
+			while(multi.compareTo(new BigInteger(Integer.toString(base - 1, base), base)) == 1) { // "multi.compareTo(" returns 1 if the number being used to compare is larger
+				multi = BigInteger.ONE;
 				for (int t = 0; t < stringTotal.length(); t++) {
-					multi = multi.multiply(new BigInteger(stringTotal.charAt(t) + "", 16));
+					multi = multi.multiply(new BigInteger(stringTotal.charAt(t) + "", check));
 				}
-				stringTotal = multi.toString(16);
+				stringTotal = multi.toString();
 				System.out.println(stringTotal);
 				result++;
-				if(multi.toString(16).length() == 1){
+				if(multi.toString().length() == 1){
 					break;
 				}
 			}			
 			System.out.println("Steps: " + result + "\n");
 			if(result > MAXSTEPS){ // Some quality of life tracking stuffs
 				MAXSTEPS = result;
-				MAXSTEPS_NUMBER = nums.toString(16);
-				System.out.println("New Max: " + MAXSTEPS + " == " + MAXSTEPS_NUMBER + " Number length: " + nums.toString(16).length() + "\n");
+				MAXSTEPS_NUMBER = nums.toString(base);
+				System.out.println("New Max: " + MAXSTEPS + " == " + MAXSTEPS_NUMBER + " Number length: " + nums.toString(base).length() + "\n");
 			}
 			nums = nums.add(BigInteger.ONE); // Adds 1 onto the number
 		}
